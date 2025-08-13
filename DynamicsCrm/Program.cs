1466874cfb71f4e06eb229086a862059c0204def
Ahmet <ozsoyyahmett@gmail.com>
2025-08-13 23:37:40 +0300

    List the Accounts table.

--- DynamicsCrm/Program.cs
+++ DynamicsCrm/Program.cs
@@ -1,37 +1,53 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.PowerPlatform.Dataverse.Client;
+﻿using Microsoft.PowerPlatform.Dataverse.Client;
 using Microsoft.Xrm.Sdk;
 using Microsoft.Xrm.Sdk.Query;
+using System;
 
-namespace DynamicsTest
+class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            string connectionString = @"
-        AuthType=ClientSecret;
-        Url=*
-        ClientId=*
-        ClientSecret=*;
-        TenantId=*;
+        string connectionString = @"
+AuthType=ClientSecret;
+Url=*;
+ClientId=*;
+ClientSecret=*;
+TenantId=*;
+
 ";
+
+        try
+        {
             using (var serviceClient = new ServiceClient(connectionString))
             {
                 if (serviceClient.IsReady)
                 {
-                    Console.WriteLine("Bağlantı başarılı.");
+                    Console.WriteLine("Bağlantı başarılı!");
+
+                    var query = new QueryExpression("account")
+                    {
+                        ColumnSet = new ColumnSet(true),
+                        TopCount = 5
+                    };
+
+                    var result = serviceClient.RetrieveMultiple(query);
+
+                    foreach (var entity in result.Entities)
+                    {
+                        Console.WriteLine($"Account: {entity.GetAttributeValue<string>("name")}");
+                    }
                 }
                 else
                 {
                     Console.WriteLine("Bağlantı başarısız.");
-                    Console.WriteLine(serviceClient.LastError);
+                    Console.WriteLine("Hata: " + serviceClient.LastError);
+                    Console.WriteLine("İç hata: " + serviceClient.LastException?.Message);
                 }
             }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Hata oluştu: " + ex.Message);
         }
     }
 }
