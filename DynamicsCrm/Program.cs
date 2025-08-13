4542e4504a4177dbdd51563940ec3e1d24ee7a42
Ahmet <ozsoyyahmett@gmail.com>
2025-08-13 23:13:56 +0300

    Microsoft Dynamics connection.

--- DynamicsCrm/Program.cs
+++ DynamicsCrm/Program.cs
@@ -3,13 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.PowerPlatform.Dataverse.Client;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
 
-namespace DynamicsCrm
+namespace DynamicsTest
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            string connectionString = @"
+        AuthType=ClientSecret;
+        Url=*
+        ClientId=*
+        ClientSecret=*;
+        TenantId=*;
+";
+            using (var serviceClient = new ServiceClient(connectionString))
+            {
+                if (serviceClient.IsReady)
+                {
+                    Console.WriteLine("Bağlantı başarılı.");
+                }
+                else
+                {
+                    Console.WriteLine("Bağlantı başarısız.");
+                    Console.WriteLine(serviceClient.LastError);
+                }
+            }
         }
     }
 }
